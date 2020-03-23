f22aff007dc7ce879f1b37e3ae24f89c40d4131a
NayanKumarG <nayangkumar@gmail.com>
2020-03-23 15:04:11 +0000

    [Nayan] Add. First commit

---
+++ FundooWebApp/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+const routes: Routes = [
+  {path:"registration" , component:RegistrationComponent},
+  {path:"" , component:RegistrationComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
