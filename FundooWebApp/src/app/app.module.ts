57602876e9b868f0ca8b98280f66862fd5d124be
NayanKumarG <nayangkumar@gmail.com>
2020-03-24 15:36:22 +0000

    [Nayan] Refactor. meta input module imported

--- FundooWebApp/src/app/app.module.ts
+++ FundooWebApp/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
@@ -21,7 +21,7 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
+    MatInputModule,
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule
