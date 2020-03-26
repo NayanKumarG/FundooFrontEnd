aefb8b099044fbddf98bc8693c1e8ae33f9fa695
NayanKumarG <nayangkumar@gmail.com>
2020-03-26 05:38:40 +0000

    [Nayan] Refactor. validation added

--- FundooWebApp/src/app/components/login/login.component.ts
+++ FundooWebApp/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -11,5 +13,20 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  email = new FormControl(null,[Validators.required,Validators.email]);
+  password= new FormControl(null,[Validators.required,Validators.minLength(8)]);
+
+  getEmailErrorMessage(){
+    return this.email.hasError('required')? "Enter Email Id":
+    this.email.hasError('email')? "EmailId not valid":
+     "";
+   }
+
+   getPasswordErrorMessage(){
+    return this.password.hasError('required')? "Enter Password":
+    this.password.hasError('minLength')? "minimum 8 characters required":
+     "";
+   }
 
 }
