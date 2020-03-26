9f3f6807f86b342d0f5ca2a2154a22351e9ba9fb
NayanKumarG <nayangkumar@gmail.com>
2020-03-26 05:37:48 +0000

    [Nayan] Refactor. validation added

--- FundooWebApp/src/app/components/forgot-password/forgot-password.component.ts
+++ FundooWebApp/src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,7 +11,17 @@ export class ForgotPasswordComponent implements OnInit {
 
   constructor() { }
 
+
+
   ngOnInit() {
   }
+
+  email = new FormControl(null,[Validators.required,Validators.email]);
+
+  getEmailErrorMessage(){
+    return this.email.hasError('required')? "Enter Email Id":
+    this.email.hasError('email')? "EmailId not valid":
+     "";
+   }
 
 }
